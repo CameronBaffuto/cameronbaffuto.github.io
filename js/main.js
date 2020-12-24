2adc156ffa62f6f81ce5e3fca59d771cb25f0b3a
Cameron Baffuto <49914962+CameronBaffuto@users.noreply.github.com>
2020-12-24 11:45:31 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ function myFunction() {
     let element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+function submitForm(form) {
+    location.href ="mailto:cambaffuto@gmail.com?subject=Hi%20Cam!&body=Hi%20my%20name%20is%20" + form.name.value + "%0D%0A" + "%20My%20message%20is...%20" + form.comment.value;
+    return false;
+}
+

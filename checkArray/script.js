0884eb53c97bbc6401a32dafd07d57276f665a9b
cameronbaffuto <cb3426@drexel.edu>
2021-02-18 00:51:02 -0500

    v2

--- checkArray/script.js
+++ checkArray/script.js
@@ -1,27 +1,22 @@
 var selected = [];
 
 function GetSelected() {
-
-        //Reference the Table.
         var tblFruits = document.getElementById("tblFruits");
 
-        //Reference all the CheckBoxes in Table.
         var chks = tblFruits.getElementsByTagName("INPUT");
 
-        // Loop and push the checked CheckBox value in Array.
         for (var i = 0; i < chks.length; i++) {
             if (chks[i].checked) {
                 selected.push(chks[i].value);
             }
         }
 
-        //Display the selected CheckBox values.
          if (selected.length > 0) {
-             alert("Selected values: " + selected.join(","));
+             // alert("Selected values: " + selected.join(","));
+             document.getElementById('vae').style.display = "block";
          }
     }
 
-    // function test(){
-    //     alert("Selected values: " + selected.join(","));
-    // }
-console.log(selected);
+    function launchVAE() {
+        alert(selected);
+    }

902d3bd177251424985d48520fbe3e7899ab951b
cameronbaffuto <cb3426@drexel.edu>
2021-03-05 18:26:08 -0500

    password

--- checkArray/script.js
+++ checkArray/script.js
@@ -1,41 +1,19 @@
-var selected = [];
-
 function GetSelected() {
-        var tblFruits = document.getElementById("tblFruits");
 
-        var chks = tblFruits.getElementsByTagName("INPUT");
+    var selected = [];
 
-        for (var i = 0; i < chks.length; i++) {
-            if (chks[i].checked) {
-                selected.push(chks[i].value);
-            }
+    var tblFruits = document.getElementById("tblFruits");
+
+    var chks = tblFruits.getElementsByTagName("INPUT");
+
+    for (var i = 0; i < chks.length; i++) {
+        if (chks[i].checked) {
+            selected.push(chks[i].value);
         }
-
-         if (selected.length > 0) {
-             // alert("Selected values: " + selected.join(","));
-             document.getElementById('vae').style.display = "block";
-         }
     }
 
-    function launchVAE() {
-        alert("'" + selected.join(",") + "'");
+    if (selected.length > 0) {
+        alert("Selected values: " + selected.join(","));
     }
+};
 
-var template = "";
-
-function GetTemplateID() {
-    com.veeva.clm.getApprovedDocument("https://vvagency-bionicalus.veevavault.com","38",GetFragmentID);
-}
-
-function GetFragmentID(result) {
-    template=result.Approved_Document_vod__c.ID;
-    com.veeva.clm.getApprovedDocument("https://vvagency-bionicalus.veevavault.com","36",SendEmail);
-
-}
-
-function SendEmail(result) {
-    com.veeva.clm.launchApprovedEmail(template,result.Approved_Document_vod__c.ID,finalCall)
-}
-
-function finalCall() {
-}

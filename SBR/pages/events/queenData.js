674729b45e79eb808f332c2a1cd06bca9453ab4e
cameronbaffuto <cb3426@drexel.edu>
2021-12-19 19:38:59 -0500

    sbr update

---
+++ SBR/pages/events/queenData.js
@@ -0,0 +1,35 @@
+const queenData = [
+    {
+        name: "Family 1",
+        hound: "Hound 1",
+        votes: "$25",
+        message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+        imgOne: "./testImg/dogs/dog1.jpg",
+        imgTwo: "./testImg/dogs/dog2.jpg",
+        imgThree: "./testImg/dogs/dog3.jpg",
+        imgFour: "./testImg/dogs/dog4.jpg",
+        imgFive: "./testImg/dogs/dog5.jpg"
+    },
+    {
+        name: "Family 2",
+        hound: "Hound 2",
+        votes: "$10",
+        message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+        imgOne: "./testImg/monkey/monkey1.jpg",
+        imgTwo: "./testImg/monkey/monkey2.jpg",
+        imgThree: "./testImg/monkey/monkey3.jpg",
+        imgFour: "./testImg/monkey/monkey4.jpg",
+        imgFive: "./testImg/monkey/monkey5.jpg"
+    },
+    {
+        name: "Family 3",
+        hound: "Hound 3",
+        votes: "$69",
+        message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+        imgOne: "./testImg/manatee/manatee1.jpg",
+        imgTwo: "./testImg/manatee/manatee2.jpg",
+        imgThree: "./testImg/manatee/manatee3.jpg",
+        imgFour: "./testImg/manatee/manatee4.jpg",
+        imgFive: "./testImg/manatee/manatee5.jpg"
+    },
+]    

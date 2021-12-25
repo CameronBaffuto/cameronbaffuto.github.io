a56369c8dc52dbbfe6b0b4450535d36ea7c94093
cameronbaffuto <cb3426@drexel.edu>
2021-12-25 12:16:26 -0500

    king comments

--- SBR/pages/events/kingData.js
+++ SBR/pages/events/kingData.js
@@ -1,35 +1,76 @@
 const kingData = [
     {
         name: "Family 1",
-        hound: "King 1",
+        hound: "King1",
         votes: "$25",
         message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
         imgOne: "./testImg/birds/bird1.jpg",
         imgTwo: "./testImg/birds/bird2.jpg",
         imgThree: "./testImg/birds/bird3.jpg",
         imgFour: "./testImg/birds/bird4.jpg",
-        imgFive: "./testImg/birds/bird5.jpg"
+        imgFive: "./testImg/birds/bird5.jpg",
+        comment: [
+            {
+                name: "Test",
+                message: "Test Comment",
+                amount: "280",
+            },
+            {
+                name: "Test",
+                message: "Test Comment",
+                amount: "151",
+            }
+          ],
     },
     {
         name: "Family 2",
-        hound: "King 2",
+        hound: "King2",
         votes: "$10",
         message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
         imgOne: "./testImg/fish/fish1.jpg",
         imgTwo: "./testImg/fish/fish2.jpg",
         imgThree: "./testImg/fish/fish3.jpg",
         imgFour: "./testImg/fish/fish4.jpg",
-        imgFive: "./testImg/fish/fish5.jpg"
+        imgFive: "./testImg/fish/fish5.jpg",
+        comment: [
+            {
+                name: "Test",
+                message: "Test Comment",
+                amount: "280",
+            },
+            {
+                name: "Test",
+                message: "Test Comment",
+                amount: "151",
+            }
+          ],
     },
     {
         name: "Family 3",
-        hound: "King 3",
+        hound: "King3",
         votes: "$69",
         message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
         imgOne: "./testImg/raccoon/raccoon1.jpg",
         imgTwo: "./testImg/raccoon/raccoon2.jpg",
         imgThree: "./testImg/raccoon/raccoon3.jpg",
         imgFour: "./testImg/raccoon/raccoon4.jpg",
-        imgFive: "./testImg/raccoon/raccoon5.jpg"
+        imgFive: "./testImg/raccoon/raccoon5.jpg",
+        comment: [
+            {
+                name: "Test",
+                message: "Test Comment",
+                amount: "280",
+            },
+            {
+                name: "Test",
+                message: "Test Comment",
+                amount: "151",
+            },
+            {
+                name: "Test",
+                message: "Test Comment",
+                amount: "151",
+            }
+          ],
     },
 ] 

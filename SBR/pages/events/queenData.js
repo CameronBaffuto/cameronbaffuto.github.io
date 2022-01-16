90bd7754f15e42bcaa2dc967f2193282dad6e6b6
cameronbaffuto <cb3426@drexel.edu>
2022-01-16 16:11:03 -0500

    map img

--- SBR/pages/events/queenData.js
+++ SBR/pages/events/queenData.js
@@ -4,11 +4,23 @@ const queenData = [
         hound: "Queen1",
         votes: "$25",
         message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-        imgOne: "./testImg/dogs/dog1.jpg",
-        imgTwo: "./testImg/dogs/dog2.jpg",
-        imgThree: "./testImg/dogs/dog3.jpg",
-        imgFour: "./testImg/dogs/dog4.jpg",
-        imgFive: "./testImg/dogs/dog5.jpg",
+        images: [
+            {
+                image: "./testImg/dogs/dog1.jpg"
+            },
+            {
+                image: "./testImg/dogs/dog2.jpg"
+            },
+            {
+                image: "./testImg/dogs/dog3.jpg"
+            },
+            {
+                image: "./testImg/dogs/dog4.jpg"
+            },
+            {
+                image: "./testImg/dogs/dog5.jpg"
+            },
+        ],
         comment: [
             {
                 name: "Test",
@@ -32,11 +44,23 @@ const queenData = [
         hound: "Queen2",
         votes: "$10",
         message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-        imgOne: "./testImg/monkey/monkey1.jpg",
-        imgTwo: "./testImg/monkey/monkey2.jpg",
-        imgThree: "./testImg/monkey/monkey3.jpg",
-        imgFour: "./testImg/monkey/monkey4.jpg",
-        imgFive: "./testImg/monkey/monkey5.jpg",
+        images: [
+            {
+                image: "./testImg/monkey/monkey1.jpg"
+            },
+            {
+                image: "./testImg/monkey/monkey2.jpg"
+            },
+            {
+                image: "./testImg/monkey/monkey3.jpg"
+            },
+            {
+                image: "./testImg/monkey/monkey4.jpg"
+            },
+            {
+                image: "./testImg/monkey/monkey5.jpg"
+            },
+        ],
         comment: [
             {
                 name: "Test",
@@ -60,11 +84,23 @@ const queenData = [
         hound: "Queen3",
         votes: "$69",
         message:  "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
-        imgOne: "./testImg/manatee/manatee1.jpg",
-        imgTwo: "./testImg/manatee/manatee2.jpg",
-        imgThree: "./testImg/manatee/manatee3.jpg",
-        imgFour: "./testImg/manatee/manatee4.jpg",
-        imgFive: "./testImg/manatee/manatee5.jpg",
+        images: [
+            {
+                image: "./testImg/manatee/manatee1.jpg"
+            },
+            {
+                image: "./testImg/manatee/manatee2.jpg"
+            },
+            {
+                image: "./testImg/manatee/manatee3.jpg"
+            },
+            {
+                image: "./testImg/manatee/manatee4.jpg"
+            },
+            {
+                image: "./testImg/manatee/manatee5.jpg"
+            },
+        ],
         comment: [
             {
                 name: "Test",

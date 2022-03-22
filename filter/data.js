4eb25f2a2d38037727ed1f6101e328f2b321c30b
cameronbaffuto <cb3426@drexel.edu>
2022-03-22 17:52:50 -0400

    data

--- filter/data.js
+++ filter/data.js
@@ -1,4 +1,14 @@
 const projectData = [
     {
+        tags: "",
+        topImg: "",
+        cardTitle: "",
+        cardText: "",
+        modalid: "",
+        modalTitle: "",
+        slideTwo: "",
+        slideThree: "",
+        slideFour: "",
+        linkButton: "#"
     }
 ];
